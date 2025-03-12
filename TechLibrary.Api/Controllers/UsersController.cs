a952575592f9d6d6d90e38581644f225e6c3d472
adonisamarante <adonisamarante@gmail.com>
2025-03-12 17:36:22 -0300

    feat: Users controller

---
+++ TechLibrary.Api/Controllers/UsersController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TechLibrary.Api.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        // IActionResult enables you to return different types of response codes(200, 404, 500, etc.)
+        [HttpPost]
+        public IActionResult Create()
+        { 
+            return Created();
+        }
+    }
+}
