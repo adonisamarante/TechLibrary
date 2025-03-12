3ac505ca26431d75b92f738f3a1303d658aa3209
adonisamarante <adonisamarante@gmail.com>
2025-03-12 17:19:36 -0300

    feat: swagger setup

--- TechLibrary.Api/Program.cs
+++ TechLibrary.Api/Program.cs
@@ -6,12 +6,18 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+// map all the endpoints and generate swagger documentation
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+// Swagger is recommended to be used only in development environment
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
