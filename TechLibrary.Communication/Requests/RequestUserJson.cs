85dadfa3a8321cf1da938b5433719846a01099c8
adonisamarante <adonisamarante@gmail.com>
2025-03-13 16:40:41 -0300

    feat: user request and response classes

---
+++ TechLibrary.Communication/Requests/RequestUserJson.cs
@@ -0,0 +1,11 @@
+﻿namespace TechLibrary.Communication.Requests
+{
+    public class RequestUserJson
+    {
+        // by default these strings will be null, this could cause errors when used in the controller
+        // to avoid this, we set them to empty strings
+        public string Name { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
