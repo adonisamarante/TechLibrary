6be4d0d9c64deae0700fa2409ca21a97c2231c20
adonisamarante <adonisamarante@gmail.com>
2025-03-18 17:46:13 -0300

    feat: create user

--- TechLibrary.Api/UseCases/Users/Register/RegisterUserUseCase.cs
+++ TechLibrary.Api/UseCases/Users/Register/RegisterUserUseCase.cs
@@ -1,4 +1,6 @@
-﻿using TechLibrary.Communication.Requests;
+﻿using TechLibrary.Api.Domain.Entities;
+using TechLibrary.Api.Infrastructure;
+using TechLibrary.Communication.Requests;
 using TechLibrary.Communication.Responses;
 using TechLibrary.Exception;
 
@@ -10,9 +12,21 @@ namespace TechLibrary.Api.UseCases.Users.Register
         {
             Validate(request);
 
+            var entity = new User
+            {
+                Name = request.Name,
+                Email = request.Email,
+                Password = request.Password
+            };
+
+            var dbContext = new TechLibraryDbContext();
+
+            dbContext.Users.Add(entity);
+            dbContext.SaveChanges();
+
             return new ResponseRegisteredUserJson
             {
-
+                Name = entity.Name,
             };
         }
 
