3d9c568dbf8874bbf7a56f0fae270a454f5e68fc
adonisamarante <adonisamarante@gmail.com>
2025-03-21 16:29:55 -0300

    feat: duplicate email validation

--- TechLibrary.Api/UseCases/Users/Register/RegisterUserUseCase.cs
+++ TechLibrary.Api/UseCases/Users/Register/RegisterUserUseCase.cs
@@ -1,4 +1,5 @@
-﻿using TechLibrary.Api.Domain.Entities;
+﻿using FluentValidation.Results;
+using TechLibrary.Api.Domain.Entities;
 using TechLibrary.Api.Infrastructure.DataAccess;
 using TechLibrary.Api.Infrastructure.Security.Cryptography;
 using TechLibrary.Communication.Requests;
@@ -11,7 +12,9 @@ namespace TechLibrary.Api.UseCases.Users.Register
     {
         public ResponseRegisteredUserJson Execute(RequestUserJson request)
         {
-            Validate(request);
+            var dbContext = new TechLibraryDbContext();
+
+            Validate(request, dbContext);
 
             var cryptography = new BCryptAlgorithm();
 
@@ -22,23 +25,26 @@ namespace TechLibrary.Api.UseCases.Users.Register
                 Password = cryptography.HashPassword(request.Password),
             };
 
-            var dbContext = new TechLibraryDbContext();
-
             dbContext.Users.Add(entity);
             dbContext.SaveChanges();
 
             return new ResponseRegisteredUserJson
             {
                 Name = entity.Name,
+                AccessToken = "token",
             };
         }
-
-        private void Validate(RequestUserJson request)
+        
+        private void Validate(RequestUserJson request, TechLibraryDbContext dbContext)
         {
             var validator = new RegisterUserValidator();
-
             var result = validator.Validate(request);
 
+            var existUserWithEmail = dbContext.Users.Any(user => user.Email.Equals(request.Email));
+
+            if (existUserWithEmail)
+                result.Errors.Add(new ValidationFailure("Email", "Email já registrado na plataforma"));
+            
             if (result.IsValid == false)
             {
                 var errorMessages = result.Errors.Select(error => error.ErrorMessage).ToList();
