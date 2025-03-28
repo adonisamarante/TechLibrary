0fd2ca3a2313731079c40f580e265f771e0b15ee
adonisamarante <adonisamarante@gmail.com>
2025-03-28 17:36:42 -0300

    feat: login use case and request json

---
+++ TechLibrary.Api/UseCases/Login/DoLogin/DoLoginUseCase.cs
@@ -0,0 +1,16 @@
+﻿using TechLibrary.Communication.Requests;
+using TechLibrary.Communication.Responses;
+
+namespace TechLibrary.Api.UseCases.Login.DoLogin
+{
+    public class DoLoginUseCase
+    {
+        public ResponseRegisteredUserJson Execute(RequestLoginJson request)
+        {
+            return new ResponseRegisteredUserJson
+            {
+
+            };
+        }
+    }
+}
