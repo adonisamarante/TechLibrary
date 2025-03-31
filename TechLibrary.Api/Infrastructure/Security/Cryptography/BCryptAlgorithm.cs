61ff4f983766b8b2759ee5d1c095f2f1529ab191
adonisamarante <adonisamarante@gmail.com>
2025-03-31 16:54:49 -0300

    feat: password verification

--- TechLibrary.Api/Infrastructure/Security/Cryptography/BCryptAlgorithm.cs
+++ TechLibrary.Api/Infrastructure/Security/Cryptography/BCryptAlgorithm.cs
@@ -1,10 +1,19 @@
-﻿namespace TechLibrary.Api.Infrastructure.Security.Cryptography
+﻿using TechLibrary.Api.Domain.Entities;
+
+namespace TechLibrary.Api.Infrastructure.Security.Cryptography
 {
     public class BCryptAlgorithm
     { 
         public string HashPassword(string password)
         {
             return BCrypt.Net.BCrypt.HashPassword(password);
+        }
+
+        // avoiding use of two parameters of the same type, instead of using "string hashPassword" to
+        // verify, you can send User and then get the password hash', it'll prevent confusion with the parameters order
+        public bool VerifyPassword(string password, User user)
+        {
+            return BCrypt.Net.BCrypt.Verify(password, user.Password);
         }
     }
 }
