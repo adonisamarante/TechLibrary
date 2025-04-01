50009b3b38ac43dfb1d35afe5f9903d62e511677
adonisamarante <adonisamarante@gmail.com>
2025-04-01 17:37:36 -0300

    feat: exception filter

---
+++ TechLibrary.Api/Filters/ExceptionFilter.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using TechLibrary.Communication.Responses;
+using TechLibrary.Exception;
+
+namespace TechLibrary.Api.Filters
+{
+    // every time there is an exception in the application, this filter will be triggered
+    // after creating this file, you need to register it in the Program.cs file, before the line "var app = builder.Build();"
+    public class ExceptionFilter : IExceptionFilter
+    {
+        public void OnException(ExceptionContext context)
+        {
+            if (context.Exception is TechLibraryException techLibraryException)
+            {
+                context.HttpContext.Response.StatusCode = (int)techLibraryException.GetStatusCode();
+                context.Result = new ObjectResult(new ResponseErrorMessagesJson
+                {
+                    Errors = techLibraryException.GetErrorMessages(),
+                });
+            }
+            else
+            {
+                context.HttpContext.Response.StatusCode = StatusCodes.Status500InternalServerError;
+                context.Result = new ObjectResult(new ResponseErrorMessagesJson
+                {
+                    Errors = ["Erro desconhecido."],
+                });
+            }
+        }
+    }
+}
