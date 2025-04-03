10f82e6b1ea6b489c701189a1e601bcdefea94eb
adonisamarante <adonisamarante@gmail.com>
2025-04-03 13:19:48 -0300

    feat: filter books use case

---
+++ TechLibrary.Api/UseCases/Books/Filter/FilterBookUseCase.cs
@@ -0,0 +1,19 @@
+﻿using TechLibrary.Api.Infrastructure.DataAccess;
+using TechLibrary.Communication.Requests;
+using TechLibrary.Communication.Responses;
+
+namespace TechLibrary.Api.UseCases.Books.Filter
+{
+    public class FilterBookUseCase
+    {
+        public ResponseBooksJson Execute(RequestFilterBooksJson request)
+        {
+            var dbContext = new TechLibraryDbContext();
+
+            return new ResponseBooksJson
+            {
+
+            };
+        }
+    }
+}
