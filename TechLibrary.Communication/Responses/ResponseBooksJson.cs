7233f14840d3627ac02d69b61f5578470ce92433
adonisamarante <adonisamarante@gmail.com>
2025-04-03 13:20:52 -0300

    feat: books responses(book, books, pagination)

---
+++ TechLibrary.Communication/Responses/ResponseBooksJson.cs
@@ -0,0 +1,10 @@
+﻿namespace TechLibrary.Communication.Responses
+{
+    public class ResponseBooksJson
+    {
+        // "default!" here is used to assure that the property is not null
+        public ResponsePaginationJson Pagination { get; set; } = default!;
+
+        public List<ResponseBookJson> Books { get; set; } = [];
+    }
+}
