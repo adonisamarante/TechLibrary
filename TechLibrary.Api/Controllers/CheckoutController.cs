13f0647f9f8e680a41c34451502194a164f4deb2
adonisamarante <adonisamarante@gmail.com>
2025-04-10 16:42:15 -0300

    feat: checkout controller and entity

---
+++ TechLibrary.Api/Controllers/CheckoutController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TechLibrary.Api.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class CheckoutController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult BookCheckout(Guid bookId)
+        {
+            return NoContent();
+        }
+    }
+}
