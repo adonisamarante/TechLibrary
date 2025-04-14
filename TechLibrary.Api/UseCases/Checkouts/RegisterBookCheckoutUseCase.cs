8ded4fb75ba033f55b8fdee457a0c3728297a3ce
adonisamarante <adonisamarante@gmail.com>
2025-04-14 16:08:44 -0300

    feat: book checkout

--- TechLibrary.Api/UseCases/Checkouts/RegisterBookCheckoutUseCase.cs
+++ TechLibrary.Api/UseCases/Checkouts/RegisterBookCheckoutUseCase.cs
@@ -1,24 +1,40 @@
 ﻿using TechLibrary.Api.Infrastructure.DataAccess;
+using TechLibrary.Api.Services.LoggedUser;
 using TechLibrary.Exception;
 
 namespace TechLibrary.Api.UseCases.Checkouts
 {
     public class RegisterBookCheckoutUseCase
     {
+        private const int MAX_LOAN_DAYS = 7;
+
+        private readonly LoggedUserService _loggedUser;
+
+        public RegisterBookCheckoutUseCase(LoggedUserService loggedUser)
+        {
+            _loggedUser = loggedUser;
+        }
+
         public void Execute(Guid bookId)
         {
             var dbContext = new TechLibraryDbContext();
 
             Validate(dbContext, bookId);
 
-            dbContext.Checkouts.Add(new Domain.Entities.Checkout
-            {
+            var user = _loggedUser.User(dbContext);
 
-            });
+            var entity = new Domain.Entities.Checkout
+            {
+                UserId = user.Id,
+                BookId = bookId,
+                ExpectedReturnDate = DateTime.UtcNow.AddDays(MAX_LOAN_DAYS),
+            };
+
+            dbContext.Checkouts.Add(entity);
 
             dbContext.SaveChanges();
         }
-
+        
         private void Validate(TechLibraryDbContext dbContext, Guid bookId)
         {
             var book = dbContext.Books.FirstOrDefault(book => book.Id == bookId);
